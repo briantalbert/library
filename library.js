c00b7cfef0b2e11a9fb701cea39b44ab427ecccd
Brian Talbert <BrianLTalbert@gmail.com>
2022-11-29 13:59:47 -0500

    Fix bug in Book creation

---
+++ library.js
@@ -0,0 +1,64 @@
+//let myShelf = []
+const myShelf = new Bookshelf()
+
+function Bookshelf() {
+    this.shelf = []
+}
+
+Bookshelf.prototype.addToPage = function(book) {
+    const parent = document.querySelector('.booklist')
+    const card = document.createElement('div')
+    const bookInfo = document.createElement('div')
+    bookInfo.classList.add('book-info')
+    card.classList.add('book-card')
+    bookInfo.appendChild(card)
+    parent.appendChild(bookInfo)
+
+    const bookTitle = document.createElement('span')
+    const bookAuthor = document.createElement('span')
+    const bookPages = document.createElement('span')
+    const bookRead = document.createElement('span')
+
+    bookTitle.classList.add('title')
+    bookAuthor.classList.add('author')
+    bookPages.classList.add('pages')
+    bookRead.classList.add('read-status')
+
+    bookTitle.textContent = book.title
+    bookAuthor.textContent = book.author
+    bookPages.textContent = book.pages + " pages"
+    bookRead.textContent = book.read ? "Read" : "Not Read Yet"
+
+    card.classList.add(book.read ? 'have-read' : 'unread')
+    card.appendChild(bookTitle)
+    card.appendChild(bookAuthor)
+    card.appendChild(bookPages)
+    card.appendChild(bookRead)
+}
+
+function Book(title, author, pages, read) {
+    this.title = title
+    this.author = author
+    this.pages = parseInt(pages)
+    this.read = read
+}
+
+Book.prototype.showInfo = function() {
+    console.log(this.title, " by ", this.author, ". " )
+}
+
+function createBookObject() {
+    infoFields = document.querySelectorAll('input')
+    const newBook = new Book(infoFields[0].value, infoFields[1].value, infoFields[2].value, infoFields[3].checked)
+    console.log(newBook.title)
+    myShelf.shelf.push(newBook)
+    myShelf.addToPage(newBook)
+}
+
+const submitButton = document.querySelector('.add')
+submitButton.addEventListener('click', (e) => { 
+    e.preventDefault()
+    createBookObject()
+})
+
+
